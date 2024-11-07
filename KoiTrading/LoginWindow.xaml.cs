89fb326eae238eb533e9f62899d4882fc52173b2
NAGH654 <huynguyen.dmx2004@gmail.com>
2024-11-07 15:49:35 +0700

    update

---
+++ KoiTrading/LoginWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KoiTrading;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        var registerWindow = new RegisterWindow();
+        registerWindow.Show();
+        this.Close();
+        e.Handled = true;
+    }
+
+    private void LoginBtn_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ KoiTrading/RegisterWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace KoiTrading;
+
+public partial class RegisterWindow : Window
+{
+    public RegisterWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        var loginWindow = new LoginWindow();
+        loginWindow.Show();
+        this.Close(); // Đóng cửa sổ đăng ký hiện tại
+        e.Handled = true;
+    }
+
+    private void SignUpBtn_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
