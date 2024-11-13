acf77e3478b21244b457ed9926b34da844a1c7bd
Khang Nguyen <nguyenpkse184168@fpt.edu.vn>
2024-11-13 22:04:39 +0700

    shopList, fishDetail, cart

---
+++ KoiTrading/FishDetail.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using KoiTradding.DAL.Models;
+
+namespace KoiTrading
+{
+    public partial class FishDetail : Window
+    {
+        private KoiFish fishItem;
+
+        public FishDetail(KoiFish selectedFish)
+        {
+            InitializeComponent();
+            fishItem = selectedFish;
+            DataContext = selectedFish;
+        }
+
+        private void AddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{fishItem.Origin} - {fishItem.Gender} has been added to your cart.", "Added to Cart", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            var shopListWindow = new ShopList();
+            shopListWindow.Show();
+            this.Close();
+        }
+    }
+}
