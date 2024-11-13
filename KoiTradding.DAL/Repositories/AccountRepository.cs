e4c76f23b7a42a396443cb24d7a468048fca6aa5
thanhdicode <thanhhdse184021@fpt.edu.vn>
2024-11-13 20:44:48 +0700

    add login

---
+++ KoiTradding.DAL/Repositories/AccountRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using KoiTradding.DAL.Models;
+
+namespace AirConditionerShop.DAL.Repositories
+{
+    public class StaffRepository
+    {
+        private KoiFishTradingContext _context;
+
+        public Account? Login(string Email, string pass)
+        {
+            _context = new();
+            return _context.Accounts.FirstOrDefault(x => x.Email == Email && x.Password == pass
+            );
+
+        }
+    }
+}
